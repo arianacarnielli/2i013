004ef3da40053937f98924f4700d6dce9e22641f
Ariana <ariana.carnielli@gmail.com>
2018-01-22 17:27:31 +0100

    deuxieme essai

---
+++ premier_essai.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 22 17:18:41 2018
+
+@author: 3525837
+"""
+
+from soccersimulator import Vector2D, SoccerState, SoccerAction
+
+from soccersimulator import Simulation, SoccerTeam, Player, show_simu
+
+from soccersimulator import Strategy
+
+from soccersimulator import settings
+
+import math
