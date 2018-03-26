04a58d73ce65d964099eba599ba29025dfe45dcf
Ariana Carnielli <ariana.carnielli@gmail.com>
2018-03-26 08:47:20 +0200

    26-03a

--- teste_optgen.py
+++ teste_optgen.py
@@ -6,6 +6,12 @@ Created on Mon Feb 26 19:07:16 2018
 """
 
 import module
+import math
 
 expe = module.ParamGenetique(2)
-expe.start_alea()
+
+
+
+lista = [0.8, 0.25, 1, 10, 0.5, 34, 35, 0.1, 50, 0.9, 2, 0.6, 0, 175, math.pi/6, 15] 
+
+expe.start_list([lista])
